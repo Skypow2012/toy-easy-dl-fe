8e01a815e8f64a3f2dbc81dd4f343e6e6db348db
gaifang <7605111+gaifang@user.noreply.gitee.com>
2020-12-22 02:08:43 +0800

    update

--- src/pages/user/register-result/locales/en-US.ts
+++ src/pages/user/register-result/locales/en-US.ts
@@ -1,8 +1,7 @@
 export default {
   'userandregister-result.login.userName': 'userName',
   'userandregister-result.login.password': 'password',
-  'userandregister-result.login.message-invalid-credentials':
-    'Invalid username or password（admin/toy.2020）',
+  'userandregister-result.login.message-invalid-credentials': 'Invalid username or password',
   'userandregister-result.login.message-invalid-verification-code': 'Invalid verification code',
   'userandregister-result.login.tab-login-credentials': 'Credentials',
   'userandregister-result.login.tab-login-mobile': 'Mobile number',
@@ -15,8 +14,7 @@ export default {
   'userandregister-result.register.get-verification-code': 'Get code',
   'userandregister-result.register.sign-in': 'Already have an account?',
   'userandregister-result.register-result.msg': 'Account：registered at {email}',
-  'userandregister-result.register-result.activation-email':
-    'The activation email has been sent to your email address and is valid for 24 hours. Please log in to the email in time and click on the link in the email to activate the account.',
+  'userandregister-result.register-result.activation-email': 'The activation email has been sent to your email address and is valid for 24 hours. Please log in to the email in time and click on the link in the email to activate the account.',
   'userandregister-result.register-result.back-home': 'Back to home',
   'userandregister-result.register-result.view-mailbox': 'View mailbox',
   'userandregister-result.navBar.lang': 'Languages',
